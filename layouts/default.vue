ee954f15b2841625f80e08f582fff8543a5fe9d8
Shai Gerbi <ShaiGerbi@gmail.com>
2021-09-22 16:16:35 +0300

    Define layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,24 @@
   <v-app dark>
     <v-main>
       <v-container>
+        <v-chip>{{ $auth.loggedIn ? $auth.user.username : 'Guest' }}</v-chip>
+
+        <div v-if="$auth.loggedIn">
+          <nuxt-link :to="{ name: 'dashboard' }">
+            Dashboard
+          </nuxt-link>
+          <v-btn @click="$auth.logout()">
+            Logout
+          </v-btn>
+        </div>
+        <div v-else>
+          <nuxt-link :to="{ name: 'login' }">
+            Login
+          </nuxt-link>
+        </div>
+
         <Nuxt />
       </v-container>
     </v-main>
   </v-app>
 </template>
-
-<script>
-export default {
-}
-</script>
