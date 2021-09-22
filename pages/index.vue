54d20eb9812d938f4d355c8353500750bab57ceb
Shai Gerbi <ShaiGerbi@gmail.com>
2021-09-22 16:21:24 +0300

    Design home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
 <template>
   <v-container>
+    <v-card>
+      <v-card-title>Home</v-card-title>
+    </v-card>
   </v-container>
 </template>
+
+<script>
+export default {
+  auth: false
+}
+</script>
